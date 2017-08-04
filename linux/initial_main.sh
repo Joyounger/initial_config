1f2eec0f107984bb095de3ef83bd3e6446b718d9
Joyounger <aquanox@163.com>
2017-08-04 21:28:26 +0800

    update linux script
    Signed-off-by: Joyounger <aquanox@163.com>

--- linux/initial_main.sh
+++ linux/initial_main.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
+function install_necessary()
+{
+	echo "install necessary packages for me"
+	sudo apt-get install ascii autojump geany meld codesearch tree
+}
 
-echo "install important packages"
-sudo apt-get install ascii autojump geany meld codesearch codesearch hardinfo
+function install_unnecessary()
+{
+	read -p "install adb and fastboot ?(y/n)" select
+	if [ $select == "y" ]; then
+		sudo apt-get install adb fastboot
+	fi
+
+	read -p "install hardinfo ?(y/n)" select
+	if [ $select == "y" ]; then
+		sudo apt-get install hardinfo
+	fi
+}
+
+function config_git()
+{
+	echo "git configuration"
+	git config --global core.quotepath false
+}
+
+install_necessary
+install_unnecessary
+config_git
 
 
 
 
 
-echo "git configuration"
-git config --global core.quotepath false
+
